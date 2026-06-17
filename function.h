5baa7d344590132c3e9d33cf44b08e24d2798624
TastyMomo <manurajbarman.ece27@jecrc.ac.in>
2026-06-17 10:01:10 +0000

    Antialiasing :p

--- function.h
+++ function.h
@@ -2,6 +2,8 @@
 #define FUNCTION_H
 
 #include <cmath>
+#include <random>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -17,6 +19,18 @@ const double pi = 3.1415926535897932385;
 //Functions
 inline double degrees_to_radian(double degrees) {
     return degrees * pi / 180.0;
+}
+
+//Random real in [0,1)
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+//Random real in [min, max)
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
 }
 
 //Headers
